6f7c630e459070c4fdb382553c01a9966ec75167
stashevskaanna24 <105846904+stashevskaanna24@users.noreply.github.com>
2022-05-26 13:31:06 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+$(function(){
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    
+    $(window).on("scroll load resize", function(){
+        scrollPos = $(this).scrollTop();
+        
+        if(scrollPos > introH){
+            header.addClass("fixed");
+            
+        }else{
+            header.removeClass("fixed");
+            
+        }
+        
+        
+    });
+    
+    
+});
